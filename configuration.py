7e04ca0368bd28842a72f32a48473d940479c5c7
LydaMatallana <matallana26@hotmail.com>
2024-06-01 21:01:48 -0500

    Version final del taller para entregar

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-753b8070-eafa-4842-ae25-97b555e16f13.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-188bc88c-ef47-4552-8c70-0dd4af5280bf.containerhub.tripleten-services.com"
 CREATE_USER_PATH = "/api/v1/users"
 KITS_PATH = "/api/v1/kits"
